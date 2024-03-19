5a2ff58fd7d7e78648355cb0bf25e366350ee7a4
qorhundred <sunyoungfb@naver.com>
2024-03-19 16:58:30 +0900

    pengggg

--- create_tables.sql
+++ create_tables.sql
@@ -12,4 +12,37 @@ CREATE TABLE Authors (
     age INT
 );
 
+-- Books 테이블 생성
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    pub_year DATETIME NOT NULL,
+    price INT NOT NULL,
+    category_id INT,
+    author_id INT,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Users 테이블 생성
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    age INT,
+    CONSTRAINT uc_name_email UNIQUE (name, email)
+);
+
+-- Purchases 테이블 생성
+CREATE TABLE Purchases (
+    purchase_id INT AUTO_INCREMENT PRIMARY KEY,
+    purchase_date DATETIME NOT NULL,
+    quantity INT NOT NULL,
+    purchase_price INT NOT NULL,
+    user_id INT,
+    book_id INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
 
