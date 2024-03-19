fb75c2f5f6248237a3b6738f37ca9985c499925a
qorhundred <sunyoungfb@naver.com>
2024-03-19 18:05:51 +0900

    epe

--- create_tables.sql
+++ create_tables.sql
@@ -45,3 +45,32 @@ CREATE TABLE Purchases (
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
+-- 1번
+SELECT name, age FROM Authors;
+-- 2번
+SELECT * FROM Books WHERE pub_year >= '2020-01-01';
+--3번
+SELECT user_id, book_id, purchase_date FROM purchases WHERE YEAR(purchase_date) = 2024;
+--4번
+SELECT name, address FROM users WHERE address = '서울';
+--5번
+SELECT * FROM Books ORDER BY price DESC;
+--6번
+SELECT * from categories WHERE category_name = '과학';
+--7번
+SELECT email, age FROM Users WHERE age >= 28 AND age <= 30;
+--8번
+Select year(pub_year) as year, COUNT(YEAR(pub_year)) as total_books From books
+GROUP BY year(pub_year) ORDER BY year asc;
+--9번
+SELECT * from purchases where quantity>= 4;
+--10
+Select books.title, categories.category_name
+from books
+inner join categories
+on books.category_id = categories.category_id;
+--11
+SELECT * FROM Users
+WHERE name LIKE '김%'; 
+
