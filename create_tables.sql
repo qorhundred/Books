84bbacfb85963705b881ec2d3acbf2387b756c62
qorhundred <sunyoungfb@naver.com>
2024-03-19 16:40:44 +0900

    Add Purchases table creation SQL

--- create_tables.sql
+++ create_tables.sql
@@ -3,3 +3,13 @@ CREATE TABLE Categories (
     category_name VARCHAR(255) NOT NULL,
     category_index INT
 );
+
+-- Authors 테이블 생성
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    age INT
+);
+
+
